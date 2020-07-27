82dcffd05d57280e4c2a4b4667f5814311a66448
Chaitanya Bankanhal <bchaitanya15@gmail.com>
2020-07-27 12:18:59 +0530

    Add file viewer dependency.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FileViewer from "react-file-viewer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    componentDidMount() {
+        let url_string = window.location.href;
+        let url = new URL(url_string);
+        this.setState({
+            loadUrl: url.searchParams.get("loadUrl"),
+            mimeType: url.searchParams.get("mimeType")
+        },()=>{
+            console.log(this.state)
+        } );
+
+    }
+
+    onError(e) {
+        console.log(e, `Error in file viewer`);
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadUrl: "",
+            mimeType: ""
+        }
+    }
+
+
+    render() {
+        const {loadUrl} = this.state;
+        const urlSplits = loadUrl ? loadUrl.split(".") : "";
+        const fileType = loadUrl ? urlSplits[urlSplits.length-1] : "";
+        return (
+            <div data-test="appComponent">
+                <FileViewer
+                    fileType={fileType}
+                    filePath={loadUrl}
+                    errorComponent={(<p>Error loading</p>)}
+                    onError={this.onError}/>
+            </div>
+        );
+    }
 }
 
 export default App;
